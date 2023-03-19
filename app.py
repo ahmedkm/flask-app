210144101270e5f081a063a5e4d411021df013ed
KAMAAL M AHMED <kamaal.m.ahmed@gmail.com>
2023-03-19 23:07:30 +0300

    changes for openshift

--- app.py
+++ app.py
@@ -8,6 +8,16 @@ def hello_world():
     return "Hello World!!"
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True,host='0.0.0.0',port=port)
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(debug=True,host='0.0.0.0',port=port)
+
+
+if 'OPENSHIFT_APP_NAME' in os.environ:              #are we on OPENSHIFT?
+    ip = os.environ['OPENSHIFT_PYTHON_IP']
+    port = int(os.environ['OPENSHIFT_PYTHON_PORT'])
+else:
+    ip = '0.0.0.0'                            #localhost
+    port = 8051
+
+httpd = make_server(ip, port, application)
